720aac39e063aca4b4ebcf7c3e0591e1df185cff
Wisnu <wisnu21cool@gmail.com>
2023-03-30 05:45:26 +0700

    acf: add options page

--- inc/acf-blocks.php
+++ inc/acf-blocks.php
@@ -80,19 +80,3 @@ function my_acf_json_save_point( $path ) {
     // return
     return $path;
 }
-
-// Options Page
-if( function_exists('acf_add_options_page') ) {
-	
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Header',
-		'menu_title'	=> 'Header',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Footer',
-		'menu_title'	=> 'Footer',
-		'parent_slug'	=> 'theme-general-settings',
-	));
-}

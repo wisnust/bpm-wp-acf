49517c3867ca11df772e192b7f2fd7e3962f38ff
Wisnu <wisnu21cool@gmail.com>
2023-03-31 00:58:27 +0700

    footer: complete header and responsive

--- footer.php
+++ footer.php
@@ -11,21 +11,86 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'best-practice-media' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'best-practice-media' ), 'WordPress' );
+<?php
+	$firstAddressState = get_field('footer_first_address_state', 'option');
+	$firstAddressAddress = get_field('footer_first_address_address', 'option');
+
+	$secondAddressState = get_field('footer_second_address_state', 'option');
+	$secondAddressAddress = get_field('footer_second_address_address', 'option');
+
+	$callText = get_field('call_text', 'option');
+	$callPhone = get_field('call_phone', 'option');
+
+	$emailText = get_field('email_text', 'option');
+	$emailAddress = get_field('email_address', 'option');
+
+	$ctaText = get_field('footer_cta_text', 'option');
+	$ctaButton = get_field('footer_cta_button', 'option');
+
+	$footerInfoText = get_field('footer_info_text', 'option');
+	
+?>
+	<footer id="colophon" class="footer">
+		<div class="container">
+			<div class="footer-main">
+				<div class="main-grid grid-1">
+					<a class="footer-logo" href="<?php echo get_home_url(); ?>">
+						<img width="326" src="<?php echo esc_url( get_header_image() ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'title' ) ); ?>" />
+					</a>
+					<div class="footer-socials">
+						<?php get_template_part( 'template-parts/global/socials' ); ?>
+					</div>
+					<div class="footer-cta">
+						<?php if ( $ctaText ): ?>
+							<h3><?php echo $ctaText; ?></h3>
+						<?php endif;?>
+						<?php if ( $ctaButton ): ?>
+								<a class="btn btn-green" href="<?php echo esc_url($ctaButton['url']); ?>" target="<?php echo esc_attr($ctaButton['target'] ?: '_self'); ?>"><?php echo esc_html($ctaButton['title']); ?> <i class="fa fa-arrow-right"></i></a>
+							<?php endif; ?>
+					</div>
+				</div>
+				<div class="main-grid grid-2">
+					<?php if ( $firstAddressState ): ?>
+						<h3><?php echo $firstAddressState; ?></h3>
+					<?php endif;?>
+					<?php if ( $firstAddressAddress ): ?>
+						<p><?php echo $firstAddressAddress; ?></p>
+					<?php endif;?>
+				</div>
+				<div class="main-grid grid-3">
+					<?php if ( $secondAddressState ): ?>
+						<h3><?php echo $secondAddressState; ?></h3>
+					<?php endif;?>
+					<?php if ( $secondAddressAddress ): ?>
+						<p><?php echo $secondAddressAddress; ?></p>
+					<?php endif;?>
+				</div>
+				<div class="main-grid grid-4">
+					<?php if ( $callText ): ?>
+						<h3><?php echo $callText; ?></h3>
+					<?php endif;?>
+					<?php if ( $callPhone ): ?>
+						<p><a href="tel:<?php echo $callPhone;?>"><?php echo $callPhone; ?></a></p>
+					<?php endif;?>
+				</div>
+				<div class="main-grid grid-5">
+					<?php if ( $emailText ): ?>
+						<h3><?php echo $emailText; ?></h3>
+					<?php endif;?>
+					<?php if ( $emailAddress ): ?>
+						<p><a href="mailto:<?php echo $emailAddress;?>"><?php echo $emailAddress; ?></a></p>
+					<?php endif;?>
+				</div>
+			</div>
+			<div class="footer-info">
+				<?php 
+					if ( $footerInfoText ):
+						echo $footerInfoText;
+					endif;
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'best-practice-media' ), 'best-practice-media', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+			</div>
+		</div>
+	</footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
